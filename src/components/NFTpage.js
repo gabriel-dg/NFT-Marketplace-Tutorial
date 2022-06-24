b97cee4444136b0dc48bdad4fdbfafa72a9637fc
GdG <54184329+gabriel-dg@users.noreply.github.com>
2022-06-24 14:09:57 -0300

    Revert "NFTpage.js - inverting NFT-owner conditional"

--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -93,9 +93,9 @@ async function buyNFT(tokenId) {
                         Seller: <span className="text-sm">{data.seller}</span>
                     </div>
                     <div>
-                    {currAddress == data.owner || currAddress == data.seller ?
-                        <div className="text-emerald-700">You are the owner of this NFT</div>
-                        : <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => buyNFT(tokenId)}>Buy this NFT</button>
+                    { currAddress == data.owner || currAddress == data.seller ?
+                        <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => buyNFT(tokenId)}>Buy this NFT</button>
+                        : <div className="text-emerald-700">You are the owner of this NFT</div>
                     }
                     
                     <div className="text-green text-center mt-3">{message}</div>
